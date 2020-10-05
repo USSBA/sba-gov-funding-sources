aed9ae76426604af2fe46b9ce9c30788c88112cb
Kevin Lee <ki.lee@sba.gov>
2020-10-05 17:19:41 -0400

    TA-000 Move ToggleExtraFields outside of enclosing function

--- js/main.js
+++ js/main.js
@@ -9,7 +9,17 @@
     console.log(event.target.value);
     // Make sure there's a valid zipcode (5 digits)
     if (event.target.value.length == 5) {
-        console.log(findSourceByZipcode(event.target.value))
+      console.log(findSourceByZipcode(event.target.value))
     }
   }
 })();
+
+function ToggleExtraFields(event) {
+  event.target.classList.toggle("right")
+  event.target.classList.toggle("down")
+  Array.from(document.getElementsByClassName("extra")).forEach(
+    function(element, index, array) {
+      element.classList.toggle("collapsed")
+    }
+  )
+}
