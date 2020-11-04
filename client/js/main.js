b3ae86b881ec3ba8eaeec1511644458fe73862b0
Kevin Lee <ki.lee@sba.gov>
2020-11-04 11:40:48 -0500

    TA-000 Add modal and mobile size table

--- client/js/main.js
+++ client/js/main.js
@@ -7,13 +7,13 @@ inputZipcode.addEventListener('input', updateValue);
 
 // Keep enter keystroke form from resetting input box
 inputZipcode.onkeypress = function(event) {
-    var key = event.charCode || event.keyCode || 0;     
+    var key = event.charCode || event.keyCode || 0;
     if (key == 13) {
       event.preventDefault();
       updateValue(event);
     }
   }
-  
+
 // Function to capture zipcode
 async function updateValue(event) {
     // Make sure there's a valid zipcode (5 digits)
@@ -98,6 +98,57 @@ function updateTable(data) {
         i += 1;
     })
 }
+
+// Translate data in JSON into DOM elements in place (without triggering a re-render)
+// function updateModal(data) {
+//   console.log('renderModal function');
+//   // Remove this once we paginate and transition it to take page #
+//   // Only take 1st 30 elements in array
+//   data = data.slice(0, 30);
+
+//   console.log(data);
+//   // Gather NodeList of all table rows
+//   var sourcesTableRows = document.querySelectorAll('#source-list tbody tr');
+//   // Iterate over all rows
+//   sourcesTableRows.forEach((row) {
+//       // Load 1 JSON object for updating DOM
+//       var dataToLoad = data[i];
+//       // Gather HTMLCollection of all table cells in this row
+//       var cells = row.children;
+//       // Iterate over all of these table cells <td> elements
+//       for (let j = 0; j < cells.length; j++) {
+//           // Turn elements in cell into proper Array
+//           var elementsInCell = Array.prototype.slice.call(cells[j].childNodes);
+//           // Find the anchor (link) element
+//           var link = elementsInCell.find(isLink);
+//           // Check for special fields that require anchor links be updated
+//           // There's an easier way to do this, but I haven't figured it out yet
+//           if (cells[j].dataset.key === "name") {
+//               // Take the URL value and make it the actual link
+//               link.href = dataToLoad['url'];
+//               // Set the innerHTML so we can include the icon easily
+//               link.innerHTML = dataToLoad['name'] + "<img src='./img/icon-external-link.svg'></img>";
+//           } else if (cells[j].dataset.key === "phone") {
+//               // Phone field is turned into a click-to-call tel: link
+//               link.href = "tel:" + dataToLoad['phone'];
+//               // Set the display text for the link to the raw phone number
+//               link.textContent = dataToLoad['phone'];
+//           } else if (cells[j].dataset.key === "url") {
+//               // Take the URL value and make it the actual link
+//               link.href = dataToLoad['url'];
+//               // Shorten to just the domain name
+//               link.textContent = link.hostname;
+//           } else {
+//               // Find the determine
+//               var contentToBeModified = elementsInCell.find(isContent);
+//               if (contentToBeModified) {
+//                   contentToBeModified.textContent = dataToLoad[cells[j].dataset.key];
+//               }
+//           }
+//       }
+//       i += 1;
+//   })
+// }
 
 // Test to see if an anchor element
 function isLink(element) {
