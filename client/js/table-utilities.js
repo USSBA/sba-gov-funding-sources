8c5253d11bd95550761579b3d6bc3140fa0d6aa2
Kevin Lee <ki.lee@sba.gov>
2020-11-17 15:05:12 -0500

    Add semi-colons

--- client/js/table-utilities.js
+++ client/js/table-utilities.js
@@ -41,20 +41,20 @@ function renderModal(event) {
   const modalCloseTrigger = document.querySelector('.popup-modal__close');
   const bodyBlackout = document.querySelector('.blackout-body');
 
-  const { modalTrigger } = event.target.dataset
-  const popupModal = document.querySelector(`[data-modal="${modalTrigger}"]`)
+  const { modalTrigger } = event.target.dataset;
+  const popupModal = document.querySelector(`[data-modal="${modalTrigger}"]`);
 
-  popupModal.classList.add('is--visible')
-  bodyBlackout.classList.add('is-blacked-out')
+  popupModal.classList.add('is--visible');
+  bodyBlackout.classList.add('is-blacked-out');
 
   popupModal.querySelector('.popup-modal__close').addEventListener('click', () => {
-    popupModal.classList.remove('is--visible')
-    bodyBlackout.classList.remove('is-blacked-out')
+    popupModal.classList.remove('is--visible');
+    bodyBlackout.classList.remove('is-blacked-out');
   })
 
   bodyBlackout.addEventListener('click', () => {
     // TODO: Turn into a function to close modal
-    popupModal.classList.remove('is--visible')
-    bodyBlackout.classList.remove('is-blacked-out')
+    popupModal.classList.remove('is--visible');
+    bodyBlackout.classList.remove('is-blacked-out');
   })
 }
